e5fbd5df79b75e90bb4d4a7917b8b49c818fa56d
Abdallah Hodieb <abdallah.hodieb@gmail.com>
2016-11-12 20:53:31 +0000

    add buttons to project and add onClick listners to display messages

--- app/src/main/java/com/abdallahhodieb/nanodegree/nanodegreeportfolio/MainActivity.java
+++ app/src/main/java/com/abdallahhodieb/nanodegree/nanodegreeportfolio/MainActivity.java
@@ -1,7 +1,10 @@
 package com.abdallahhodieb.nanodegree.nanodegreeportfolio;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClickProject1(View v) {
+        displayMessage("Project 1");
+    }
+
+    public void onClickProject2(View v) {
+        displayMessage("Project 2");
+    }
+
+    public void onClickProject3(View v) {
+        displayMessage("Project 3");
+    }
+
+    public void onClickProject4(View v) {
+        displayMessage("Project 4");
+    }
+
+    public void onClickProject5(View v) {
+        displayMessage("Project 5");
+    }
+
+    public void displayMessage(String message) {
+        Toast toast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
 }
+
