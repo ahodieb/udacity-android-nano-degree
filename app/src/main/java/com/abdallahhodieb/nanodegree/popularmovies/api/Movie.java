0579be0b425c57e0683bebc2e866062400a53379
Abdallah Hodieb <hodieba@amazon.com>
2016-12-09 03:01:10 +0000

    add release date conversion

--- app/src/main/java/com/abdallahhodieb/nanodegree/popularmovies/api/Movie.java
+++ app/src/main/java/com/abdallahhodieb/nanodegree/popularmovies/api/Movie.java
@@ -1,16 +1,30 @@
 package com.abdallahhodieb.nanodegree.popularmovies.api;
 
+import android.util.Log;
+
 import com.google.gson.annotations.SerializedName;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Getter;
 
 @Data
 public class Movie {
-    private String id;
+    private final  static SimpleDateFormat releaseDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    private final static String LOG_TAG = Movie.class.getName();
+
+    private long id;
     private String title;
+    private String popularity;
+    private String runtime;
 
     @SerializedName("release_date")
-    private String releaseDate;
+    @Getter(AccessLevel.NONE)
+    private String releaseDateString;
 
     @SerializedName("vote_average")
     private String rating;
@@ -18,16 +32,20 @@ public class Movie {
     @SerializedName("overview")
     private String plot;
 
-    @SerializedName("backdrop_path")
-    private String backdropPath;
-
     @SerializedName("poster_path")
     private String posterPath;
 
-    private String popularity;
-
     public String getPosterImagePath(PosterSize size) {
         return String.format("https://image.tmdb.org/t/p/%s%s", size.toString(), posterPath);
+    }
+
+    public Date getReleaseDate() {
+        try {
+            return releaseDateFormat.parse(releaseDateString);
+        } catch (ParseException e) {
+            Log.e(LOG_TAG, "Failed to parse release date", e);
+            return null;
+        }
     }
 
     public enum PosterSize {
