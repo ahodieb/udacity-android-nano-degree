256bdcf468897c082a55c42793301a12967feef4
Abdallah Hodieb <hodieba@amazon.com>
2016-12-09 03:13:25 +0000

    Fetch details from api and fall back to json object

--- app/src/main/java/com/abdallahhodieb/nanodegree/popularmovies/MovieDetailsActivity.java
+++ app/src/main/java/com/abdallahhodieb/nanodegree/popularmovies/MovieDetailsActivity.java
@@ -1,6 +1,7 @@
 package com.abdallahhodieb.nanodegree.popularmovies;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,8 +12,17 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.abdallahhodieb.nanodegree.popularmovies.api.Movie;
+import com.abdallahhodieb.nanodegree.popularmovies.api.TMDBService;
 import com.google.gson.Gson;
 import com.squareup.picasso.Picasso;
+
+import java.util.Calendar;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static com.abdallahhodieb.nanodegree.popularmovies.api.ApiUtil.createApiService;
 
 public class MovieDetailsActivity extends AppCompatActivity {
 
@@ -23,16 +33,36 @@ public class MovieDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
         setupToolbar();
+        setupAddToFavouritsButton();
 
-        Movie movie = getMovieDataFromIntent();
-        fillData(movie);
+        final Movie movie = getMovieDataFromIntent();
+        fillMovieDetailsFromService(movie);
+    }
 
+    private void setupAddToFavouritsButton() {
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Not implemented in Stage 1", Snackbar.LENGTH_SHORT)
                         .setAction("Action", null).show();
+            }
+        });
+    }
+
+    private void fillMovieDetailsFromService(final Movie movie) {
+        TMDBService service = createApiService();
+        Call<Movie> getMovieDetails = service.getMovieDetails(movie.getId(), BuildConfig.TMDB_API_KEY);
+
+        getMovieDetails.enqueue(new Callback<Movie>() {
+            @Override
+            public void onResponse(Call<Movie> call, Response<Movie> response) {
+                fillData(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<Movie> call, Throwable t) {
+                fillData(movie);
             }
         });
     }
@@ -50,19 +80,34 @@ public class MovieDetailsActivity extends AppCompatActivity {
         title.setText(movie.getTitle());
 
         TextView rating = (TextView) findViewById(R.id.movieRating);
-        rating.setText(movie.getRating());
+        if(movie.getRuntime() != null) {
+            Resources res = getResources();
+            String ratingText = String.format(res.getString(R.string.rating_format), movie.getRating());
+            rating.setText(ratingText);
+        }
 
         TextView duration = (TextView) findViewById(R.id.movieDuration);
+        if(movie.getRuntime() != null) {
+            Resources res = getResources();
+            String durationText = String.format(res.getString(R.string.duration_format), movie.getRuntime());
+            duration.setText(durationText);
+        }
 
         TextView year = (TextView) findViewById(R.id.movieYear);
-        year.setText(movie.getReleaseDate());
+        if(movie.getReleaseDate() != null ) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(movie.getReleaseDate());
+            year.setText(calendar.get(Calendar.YEAR) + "");
+        }
 
         ImageView moviePoster = (ImageView) findViewById(R.id.moviePoster);
-        Picasso.with(getBaseContext()).load(movie.getPosterImagePath(Movie.PosterSize.w185)).into(moviePoster);
+        Picasso.with(getBaseContext())
+                .load(movie.getPosterImagePath(Movie.PosterSize.w185))
+                .error(R.drawable.not_available)
+                .into(moviePoster);
 
         TextView plot = (TextView) findViewById(R.id.moviePlot);
         plot.setText(movie.getPlot());
-
     }
 
     private Movie getMovieDataFromIntent() {
